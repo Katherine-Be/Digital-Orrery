441dd9675d73af29b5deb301c2748fff0afbe87e
Katherine <katherine.bensley.93@gmail.com>
2024-06-25 18:47:16 -0400

    export default defindConfig

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 // vite.config.js
-export default {
-    // config options
-    base: '/Trying_THREEJS/'
-  }
+// export default {
+//     // config options
+//     // base: '/Trying_THREEJS/'
+//   }
+
+export default defineConfig({
+    base: "/Trying_THREEJS/",
+    plugins: [],
+    resolve: {
+      alias: [
+        
+      ]
+    },
+  })
